86a9ac998a5424abc3a09b83be2cc37ed4593ef8
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-06-27 17:48:15 -0400

    avance

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-/* Holiii acá va tu código también */
+window.onload = () =>{
+const btn = document.getElementById("boton");
+
+btn.addEventListener("click", ()=> {
+  let mailAdd = document.getElementById("exampleInputEmail1").value;
+  let passwordAdd = document.getElementById("exampleInputPassword1").value;
+  if (passwordAdd == Number || passwordAdd.length <= 8  ){
+    window.location.href='home.html';
+}else{
+  return alert("no es una contraseña valida")
+  
+}
+ 
+})
+}
